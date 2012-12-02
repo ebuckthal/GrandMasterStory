4f8f1d0b7375a03b5d10aab71e6ceeb021d1fb58
Connor Lange <rclange@calpoly.edu>
2012-12-02 15:31:19 -0800

    Added pseudocode for features

--- plot/features.py
+++ plot/features.py
@@ -1,5 +1,33 @@
-DEATH = "death"
+# Feature Enums
+PATH = "death"
 CHECK = "check"
 DEFEAT = "checkMate"
 TRAVEL = "travel"
 TRAVEL_FAR = "travelFar"
+DRAMATIC = "dramatic"
+
+
+def getWeightVector(game): 
+  weightVector = []
+  featureWeights = {}
+  # for move in game.moves()
+  #   featureWeights[DRAMATIC] = getDramaticWeight(move)
+  #   featureWeights[DEATH] = getDeathWeight(move)
+  #   etc. for all features
+  #   weightVector.append(featureWeights)
+  #   featureWeights.clear()
+
+  return weightVector
+
+def getDramaticWeight(move):
+  pass
+
+def getDeathWeight(move):
+  pass
+
+def getTravelWeight(move):
+  pass 
+
+# 
+# more feature weightings 
+# 
