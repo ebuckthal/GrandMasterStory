ec706557811dcd7708d0fdf805dce615324010de
Mike Buerli <mbuerli@gmail.com>
2012-12-02 14:04:46 -0800

    added gamefeatures

--- plot/features.py
+++ plot/features.py
@@ -3,3 +3,15 @@ CHECK = "check"
 DEFEAT = "checkMate"
 TRAVEL = "travel"
 TRAVEL_FAR = "travelFar"
+
+#game features
+DEATHOF = "@DEATHOF"
+
+#PieceTypes
+PAWN = "@PAWN"
+ROOK = "@ROOK"
+KNIGHT = "@KNIGHT"
+BISHOP = "@BISHOP"
+QUEEN = "@QUEEN"
+KING = "@KING"
+
