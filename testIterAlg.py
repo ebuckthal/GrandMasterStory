b09f83e22edee296d4a05e158046624d12a110f1
Mike Buerli <mbuerli@gmail.com>
2012-12-03 17:37:59 -0800

    cleanup

--- testIterAlg.py
+++
@@ -1,7 +0,0 @@
-from plot.iterator import PlotIterator
-import skins.WarStory as skin
-import chess.game
-
-game = chess.game.randomGame()
-iterator = PlotIterator(skin.plot, game)
-print iterator.generateStory(debug=True)
