631ad2611a62a7a97649e98844a3f2488e8cdf16
Nick Feeney <nickfeeney@gmail.com>
2012-12-04 14:28:43 -0800

    Zomibes skin update

--- skins/Zombie.py
+++ skins/Zombie.py
@@ -49,63 +49,64 @@ out of the @BUILDING they were hiding in to find food and ammunition."],
 #SAFETY
 ["@ALLCHAR stumble exaushed on to the boat and sail off the coast leaving behind the hordes of zomibes trying to eat them."],
 #Defeat
-["Suddenly the group is cornered by the biggest horde of zombies that they have ever seen lead by a @ZOMBIE. @ALLCHAR are torn apart brutally and killed."]
+["Suddenly the group is cornered by the biggest horde of zombies that they have ever seen lead by a @ZOMBIE. @ALLCHAR are torn apart brutally and killed."],
 #boarding
-["A cloud moved to block the moon. It got unbarably dark."]
+["A cloud moved to block the moon. It got unbarably dark."],
 #boarding
-["The group heard a growl and froze."]
+["The group heard a growl and froze."],
 #important kill, Travel
-["
+["As the group ran through the street, @ALIVECHAR shot a @ZOMBIE in the face killing it."],
 #CHECK, important kill
+["They where less the 300 yards to the boat that was there safety when a @ZOMBIE jumped them. @ALIVECHAR shot his crossbow and hit the zombie in the eye."],
 #Dramatic, important kill
-#danager, dramatic
+["@ALIVECHAR shot a gas tank attached to a car which explosed killing many zomibes."],
 #important kill, unimportant death
-
+["@ALIVECHAR was badly wounded in the arm while killing a zombie"]
 ]
 
 nodes = [
   ("0 Intro", \
-    [1,2,3,4,5,6,7,8,9,10,11,12], \
+    [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18], \
     []), \
   
   ("1 Boring...", \
-    [1,2,3,4,5,6,7,8,9,10,11,12], \
+    [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18], \
     []), \
   
   ("2 danger.", \
-    [1,2,3,4,5,6,7,8,9,10,11,12], \
+    [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18], \
     [features.DANGER]), \
   
   ("3 dramatic.", \
-    [1,2,3,4,5,6,7,8,9,10,11,12], \
+    [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18], \
     [features.DRAMATIC]), \
   
   ("4 hero .", \
-    [1,2,3,4,5,6,7,8,9,10,11,12], \
+    [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18], \
     [features.HERO, features.IMPORTANT_KILL, features.UNIMPORTANT_KILL]), \
   
   ("5 important kill .", \
-    [1,2,3,4,5,6,7,8,9,10,11,12], \
+    [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18], \
     [features.IMPORTANT_KILL]), \
   
   ("6 UNIMPORTANT_KILL", \
-    [1,2,3,4,5,6,7,8,9,10,11,12], \
+    [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18], \
     [features.UNIMPORTANT_KILL]), \
   
   ("7 IMPORTANT_DEATH", \
-    [1,2,3,4,5,6,7,8,9,10,11,12], \
+    [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18], \
     [features.IMPORTANT_DEATH]), \
   
   ("8 UNIMPORTANT_DEATH", \
-    [1,2,3,4,5,6,7,8,9,10,11,12], \
+    [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18], \
     [features.UNIMPORTANT_DEATH]), \
   
   ("9 TRAVEL", \
-    [1,2,3,4,5,6,7,8,9,10,11,12], \
+    [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18], \
     [features.TRAVEL]), 
   
   ("10 check", \
-    [1,2,3,4,5,6,7,8,9,10,11,12], \
+    [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18], \
     [features.CHECK]),
   
   ("11 SAFETY", \
@@ -114,7 +115,25 @@ nodes = [
   
   ("12 defeat", \
     None, \
-    [features.DEFEAT])
+    [features.DEFEAT]),
+  ("13 Boring...2", \
+    [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18], \
+    []), \
+  ("14 Boring...3", \
+    [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18], \
+    []), \
+  ("15 Important Kill, Travel", \
+    [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18], \
+    [features.IMPORTANT_KILL, features.TRAVEL]), \
+  ("16 Check Important Kill", \
+    [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18], \
+    [features.CHECK, features.IMPORTANT_KILL]), \
+  ("17 Dramatic, Important kill", \
+    [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18], \
+    [features.DRAMATIC, features.IMPORTANT_KILL]), \
+  ("18 Important Kill Unimportant death", \
+    [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18], \
+    [features.IMPORTANT_KILL, features.UNIMPORTANT_DEATH]), \
 ]
 
 plot = skin.initPlot(nodes, templates, wordset, constants, choices, resources)
